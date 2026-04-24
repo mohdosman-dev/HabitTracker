e6bfec525d256a79b49815763dfae21e4f7345da
Mohammed Osman <mohammed.osman@vianeos.com>
2026-04-24 17:48:54 +0400

    Add core domain and presentation utilities (missing from previous commit)

---
+++ app/src/main/java/com/otaku/habittracker/core/domain/Result.kt
@@ -0,0 +1,47 @@
+package com.otaku.habittracker.core.domain
+
+interface Error
+
+sealed interface Result<out D, out E : Error> {
+    data class Success<out D>(val data: D) : Result<D, Nothing>
+    data class Error<out E : com.otaku.habittracker.core.domain.Error>(val error: E) : Result<Nothing, E>
+}
+
+typealias EmptyResult<E> = Result<Unit, E>
+
+inline fun <T, E : Error, R> Result<T, E>.map(
+    map: (T) -> R
+): Result<R, E> {
+    return when (this) {
+        is Result.Error -> Result.Error(error)
+        is Result.Success -> Result.Success(map(this.data))
+    }
+}
+
+inline fun <T, E : Error> Result<T, E>.onSuccess(
+    action: (T) -> Unit
+): Result<T, E> {
+    return when (this) {
+        is Result.Error -> this
+        is Result.Success -> {
+            action(this.data)
+            this
+        }
+    }
+}
+
+inline fun <T, E : Error> Result<T, E>.onFailure(
+    action: (E) -> Unit
+): Result<T, E> {
+    return when (this) {
+        is Result.Error -> {
+            action(error)
+            this
+        }
+        is Result.Success -> this
+    }
+}
+
+fun <T, E : Error> Result<T, E>.asEmptyResult(): EmptyResult<E> {
+    return map { }
+}
